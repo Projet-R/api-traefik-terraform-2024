f9f12f3b1c2ca15d92c7b59ef18384f31c5db733
arno-yv <152905319+arno-yv@users.noreply.github.com>
2024-02-02 16:32:45 +0100

    Update iam.tf eks

--- iam.tf
+++ iam.tf
@@ -2,7 +2,7 @@
 resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = []
-  url             = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
+  url             = aws_eks_cluster.eks.identity.0.oidc.0.issuer
 }
 
 # Configuration du rôle IAM pour les nœuds EC2 du cluster EKS

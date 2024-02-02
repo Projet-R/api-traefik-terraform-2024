7f9bf7bef2f143a5ae90ac665332432ba01653a8
arno-yv <152905319+arno-yv@users.noreply.github.com>
2024-02-02 18:03:09 +0100

    Update eks.tf add version Addon

--- eks.tf
+++ eks.tf
@@ -22,6 +22,7 @@ resource "aws_eks_cluster" "eks" {
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name                = aws_eks_cluster.eks.name
   addon_name                  = "aws-ebs-csi-driver"
+  addon_version               = "v1.27.0-eksbuild.1"
   service_account_role_arn    = aws_iam_role.eks_ebs_csi.arn
   depends_on = [
     aws_iam_policy_attachment.ebs_csi_controller

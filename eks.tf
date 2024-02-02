80f4f0877cc1d6cc36d5ca31f5452e62290b3652
arno-yv <152905319+arno-yv@users.noreply.github.com>
2024-02-02 16:57:18 +0100

    Update eks.tf test

--- eks.tf
+++ eks.tf
@@ -22,10 +22,10 @@ resource "aws_eks_cluster" "eks" {
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name                = aws_eks_cluster.eks.name
   addon_name                  = "aws-ebs-csi-driver"
-  service_account_role_arn    = aws_iam_role.eks_ebs_csi.arn
-  depends_on = [
-    aws_iam_policy_attachment.ebs_csi_controller
-  ]
+  #service_account_role_arn    = aws_iam_role.eks_ebs_csi.arn
+  #depends_on = [
+  #  aws_iam_policy_attachment.ebs_csi_controller
+  #]
 }
 
 # Configuration du groupe de nœuds EC2 pour le cluster EKS

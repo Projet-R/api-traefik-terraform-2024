51be77d10a73252833a67b860197b4ce2f236e17
Arnaud Yvinec <canu13@hotmail.fr>
2024-02-02 18:23:49 +0000

    modif fichier eks

--- eks.tf
+++ eks.tf
@@ -20,10 +20,10 @@ resource "aws_eks_cluster" "eks" {
 
 # Ajout de l'add-on EBS CSI pour EKS
 resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name                = aws_eks_cluster.eks.name
-  addon_name                  = "aws-ebs-csi-driver"
-  addon_version               = "v1.27.0-eksbuild.1"
-  service_account_role_arn    = aws_iam_role.eks_ebs_csi.arn
+  cluster_name             = aws_eks_cluster.eks.name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.27.0-eksbuild.1"
+  service_account_role_arn = aws_iam_role.eks_ebs_csi.arn
   depends_on = [
     aws_iam_policy_attachment.ebs_csi_controller
   ]

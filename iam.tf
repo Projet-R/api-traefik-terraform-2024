405572890d4e2c9ff08f1186b3e3d4dd5c079ccf
Arnaud Yvinec <canu13@hotmail.fr>
2024-02-03 12:59:04 +0000

    change OIDC IAM 2

--- iam.tf
+++ iam.tf
@@ -92,8 +92,8 @@ resource "aws_iam_role" "eks_ebs_csi" {
       "Action": "sts:AssumeRoleWithWebIdentity",
       "Condition": {
         "StringEquals": {
-          "${replace(aws_iam_openid_connect_provider.oidc_eks.url, "https://", "")}:aud" = "sts.amazonaws.com",
-          "${replace(aws_iam_openid_connect_provider.oidc_eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          "${replace(aws_iam_openid_connect_provider.oidc_eks.url, "https://", "")}:aud": "sts.amazonaws.com",
+          "${replace(aws_iam_openid_connect_provider.oidc_eks.url, "https://", "")}:sub": "system:serviceaccount:kube-system:aws-load-balancer-controller"
         }
       }
     }

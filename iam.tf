06ed76b599afd04d5ab1d2c52b4f82994b7bb417
Arnaud Yvinec <canu13@hotmail.fr>
2024-02-03 13:04:27 +0000

    modif IDC

--- iam.tf
+++ iam.tf
@@ -87,13 +87,13 @@ resource "aws_iam_role" "eks_ebs_csi" {
     {
       "Effect": "Allow",
       "Principal": {
-        "Federated": "arn:aws:iam::203271543287:oidc-provider${replace(aws_iam_openid_connect_provider.oidc_eks.url, "https://", "")}"
+        "Federated": "arn:aws:iam::203271543287:oidc-provider/${replace(aws_iam_openid_connect_provider.oidc_eks.url, "https://", "")}"
       },
       "Action": "sts:AssumeRoleWithWebIdentity",
       "Condition": {
         "StringEquals": {
           "${replace(aws_iam_openid_connect_provider.oidc_eks.url, "https://", "")}:aud": "sts.amazonaws.com",
-          "${replace(aws_iam_openid_connect_provider.oidc_eks.url, "https://", "")}:sub": "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          "${replace(aws_iam_openid_connect_provider.oidc_eks.url, "https://", "")}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
         }
       }
     }

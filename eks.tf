f95aea6ee986ede795b662307ebd5969b996e425
Arnaud Yvinec <canu13@hotmail.fr>
2024-02-03 11:43:06 +0000

    add provider helm

--- eks.tf
+++ eks.tf
@@ -59,6 +59,22 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   ]
 }
 
+data "aws_eks_cluster" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    token                  = data.aws_eks_cluster_auth.eks.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  }
+}
+
 # Ajout de l'add-on ALB pour EKS
 
 resource "helm_release" "alb-controller" {

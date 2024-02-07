8e9d7b702b1ae09618bd0c0885fae413d6110821
Arnaud Yvinec <canu13@hotmail.fr>
2024-02-07 09:04:10 +0000

    morning push 6

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks.name]
     command     = "aws"
   }
@@ -44,7 +44,7 @@ provider "helm" {
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks.name]
       command     = "aws"
     }

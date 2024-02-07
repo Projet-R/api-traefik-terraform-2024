ff2c1568cccc5583c7024aa6353a5e9292b497b8
Arnaud Yvinec <canu13@hotmail.fr>
2024-02-07 08:07:01 +0000

    morning push 4

--- eks.tf
+++ eks.tf
@@ -69,6 +69,7 @@ data "aws_eks_cluster_auth" "eks" {
 }
 
 provider "kubernetes" {
+  load_config_file       = false
   host                   = data.aws_eks_cluster.eks.endpoint
   token                  = data.aws_eks_cluster_auth.eks.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
@@ -76,6 +77,7 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
+    load_config_file       = false
     host                   = data.aws_eks_cluster.eks.endpoint
     token                  = data.aws_eks_cluster_auth.eks.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
